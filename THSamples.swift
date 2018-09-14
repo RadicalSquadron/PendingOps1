6bae6a0f527ec15a312873cc683e4a062ffa4739
RadicalSquadron <rajesh040.rj@gmail.com>
2018-09-14 10:44:00 +0530

    With parsing2

--- THSamples.swift
+++ THSamples.swift
@@ -96,4 +96,111 @@ struct Article: Decodable {
  for article in blog.articles {
  print("- \(article.title)")
  }
+ ------------------trail
+ 
+ 
+ struct Employee: Decodable{
+ let id: Int
+ let employeeName: String
+ let newArticle: [Article]
+ 
+ enum CodingKeys: String, CodingKey
+
+ 
+ }
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ //
+ //  DZNativeConnection.swift
+ //  DataZoomApp
+ //
+ //  Created by iOS Developer on 21/03/18.
+ //  Copyright © 2018 iOS Developer. All rights reserved.
+ //devplatform.datazoom.io:8080/beacon/v1/config?configuration_id=d5465a70-aa31-4966-a1ad-7dd11c35709f
+ 
+ import UIKit
+ enum API
+ {
+ static let mainURL = "https://devplatform.datazoom.io/beacon/v1/config?configuration_id=87767795-ad8e-4cd0-a04f-147289ceb399"
+ }
+ typealias ServiceResponse = (Data, Error?) -> Void
+ 
+ 
+ open class DZNativeConnection :NSObject{
+ 
+ static let dzNC = DZNativeConnection()
+ 
+ let reachability = Reachability()!
+ 
+ func getTheAPIToWork(method: String, onCompletion: @escaping (Data) -> Void) {
+ if reachability.connection != .none {
+ let route = API.mainURL
+ if method == "post"{
+ makeHTTPGetRequest(path: route, onCompletion: { data, err in
+ onCompletion(data as Data)
+ })
+ }
+ else{
+ //change
+ makeHTTPGetRequest(path: route, onCompletion: { data, err in
+ onCompletion(data as Data)
+ 
+ })
+ }
+ }
+ else{
+ print("No internet")
+ }
+ 
+ }
+ 
+ 
+ func makeHTTPGetRequest(path: String, onCompletion: @escaping ServiceResponse) {
+ let request = NSMutableURLRequest(url: NSURL(string: path)! as URL)
+ 
+ let session = URLSession.shared
+ 
+ let task = session.dataTask(with: request as URLRequest, completionHandler: {data, response, error -> Void in
+ onCompletion(data!, error)
+ //            print(data!)
+ })
+ task.resume()
+ }
+ 
+ func makeHTTPPostRequest(path: String, body: [String: AnyObject], onCompletion: @escaping ServiceResponse) {
+ let request = NSMutableURLRequest(url: NSURL(string: path)! as URL)
+ 
+ // Set the method to POST
+ request.httpMethod = "POST"
+ let jsonData = try? JSONSerialization.data(withJSONObject: body)
+ 
+ // Set the POST body for the request
+ request.httpBody =  jsonData
+ let session = URLSession.shared
+ 
+ let task = session.dataTask(with: (request as URLRequest) as URLRequest, completionHandler: {data, response, error -> Void in
+ onCompletion(data!, error)
+ })
+ task.resume()
+ }
+ 
+ 
+ }
+ 
+
+ 
+ 
+ 
 */
+
+
+
+
+
+
+
